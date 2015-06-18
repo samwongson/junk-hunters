54ad3421b777c9ec6bcca13152badc3f03ef0278
Samson Wong <samsonw_@hotmail.com>
2015-06-18 03:42:36 +0000

    image uploader

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,10 +1,17 @@
+
+
+
 class Sale < ActiveRecord::Base
+  
+
   belongs_to :user
   has_many :items
 
   validates :address, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
+
+  mount_uploader :image_path, ImageUploader
 
 
 
