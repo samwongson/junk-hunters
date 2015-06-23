025b1c278ffa16feb409e83c3f987bc49755cbb3
Samson Wong <samsonw_@hotmail.com>
2015-06-23 00:42:34 +0000

    timezone

--- db/migrate/20150619012315_start_end_time_to_datestamp.rb
+++ db/migrate/20150619012315_start_end_time_to_datestamp.rb
@@ -1,6 +1,6 @@
 class StartEndTimeToDatestamp < ActiveRecord::Migration
   def change
-    change_column :sales, :start_time, :datetime
-    change_column :sales, :end_time, :datetime
+    change_column :sales, :start_time, 'datetime with time zone'
+    change_column :sales, :end_time, 'datetime with time zone'
   end
 end
